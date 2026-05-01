767864053c477281d7f1beaaa436a270d3c4f2e9
Bogdan <mail@mail.co>
2026-05-01 19:57:37 +0300

    bug fix v2 (settings)

--- shop/settings.py
+++ shop/settings.py
@@ -34,8 +34,8 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'domain.com', 'www.domain.com']
 CSRF_TRUSTED_ORIGINS = [
     'http://127.0.0.1:8000',
     'http://localhost:8000',
-    'http://domain.com:8000',
-    'http://www.domain.com:8000',
+    'https://domain.com:8000',
+    'https://www.domain.com:8000',
 ]
 
 CSRF_COOKIE_SECURE = True
